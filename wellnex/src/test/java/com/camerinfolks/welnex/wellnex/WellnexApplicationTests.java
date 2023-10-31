bf8dd46a3f9bdb3fab796758804348c798b4fe7f
MrUnwonted <arjungodzarjun@gmail.com>
2023-10-31 15:55:46 +0530

    first commit

---
+++ wellnex/src/test/java/com/camerinfolks/welnex/wellnex/WellnexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camerinfolks.welnex.wellnex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellnexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

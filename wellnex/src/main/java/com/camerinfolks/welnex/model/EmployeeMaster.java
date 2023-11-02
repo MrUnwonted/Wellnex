32367747854e01b294c18cde23b2ee400be669ea
MrUnwonted <arjungodzarjun@gmail.com>
2023-11-02 14:00:42 +0530

    Changed String to Number

--- wellnex/src/main/java/com/camerinfolks/welnex/model/EmployeeMaster.java
+++ wellnex/src/main/java/com/camerinfolks/welnex/model/EmployeeMaster.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Entity
 @AllArgsConstructor
@@ -18,36 +18,36 @@ public class  EmployeeMaster {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Number id;
     @Column(name = "STATUS",nullable = false)
-    private String status;
+    private Number status;
     @Column(name = "DATE_OF_BIRTH",nullable = false)
-    private Date dateOfBirth;
+    private LocalDate dateOfBirth;
     @Column(name = "DESIGNATION",nullable = false)
-    private String designation;
+    private Number designation;
     @Column(name = "DATE_OF_JOIN",nullable = false)
-    private Date dateOfJoin;
+    private LocalDate dateOfJoin;
     @Column(name = "PREFIX",nullable = false)
-    private String prefix;
+    private Number prefix;
     @Column(name = "LAST_NAME",nullable = false)
     private String lastName;
-    @Column(name = "DEPARTMENT",nullable = false)
-    private String department;
+    @Column(name = "DEPARTMENT_ID",nullable = false)
+    private Number department;
     @Column(name = "FIRST_REPORTING_AUTHORITY",nullable = false)
-    private String firstReportingAuthority;
-    @Column(name = "BRANCH",nullable = false)
-    private String branch;
+    private Number firstReportingAuthority;
+    @Column(name = "BRANCH_ID",nullable = false)
+    private Number branchId;
     @Column(name = "EMPLOYEE_TYPE",nullable = false)
-    private String employeeType;
-    @Column(name = "LEAVE_RULE",nullable = false)
-    private String leaveRule;
+    private Number employeeType;
+    @Column(name = "LEAVERULETYPE_ID",nullable = false)
+    private Number leaveRuleTypeId;
     @Column(name = "FIRST_NAME",nullable = false)
     private String firstName;
-    @Column(name = "GENDER",nullable = false)
-    private String gender;
+    @Column(name = "GENDERS",nullable = false)
+    private Number genders;
     @Column(name = "MOBILE_NUMBER",nullable = false,unique = true)
     private Integer mobileNumber;
     @Column(name = "EMPLOYEE_CATEGORY",nullable = false)
-    private String employeeCategory;
+    private Number employeeCategory;
 
 }
